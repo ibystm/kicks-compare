20161cdc53671d724bef38a254ab3abc02c59b1a
高橋康友 <>
2019-06-24 10:34:14 +0900

    無駄な余白削除　app/Models/User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for arrays.
      *
-     * @var array 
+     * @var array
      */
     protected $hidden = [
         'password',
