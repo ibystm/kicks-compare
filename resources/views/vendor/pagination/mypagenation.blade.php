c36bfdc019491f25e256225ae6624fb949a6e781
高橋康友 <>
2019-06-27 11:52:05 +0900

    pagenationの雛形修正　flatuiに合うように

--- resources/views/vendor/pagination/mypagenation.blade.php
+++ resources/views/vendor/pagination/mypagenation.blade.php
@@ -1,46 +1,34 @@
 @if ($paginator->hasPages())
-    <ul role="navigation">
-        {{-- Previous Page Link --}}
+    <div class="pagination pagination-inverse" style="width: 100%;">
         @if ($paginator->onFirstPage())
-            <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-              <a class="btn btn-inverse previous" href="">OLDER</a>
-            </li>
+            <a class="btn btn-inverse previous disabled" href="#">OLDER</a>
         @else
-            <li>
-                <a class="btn btn-inverse previous" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">OLDER</a>
-            </li>
+            <a class="btn btn-inverse previous" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">OLDER</a>
         @endif
-
-        {{-- Pagination Elements --}}
         @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+            <li class="disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
             @endif
 
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
-                    @else
-                        <li><a href="{{ $url }}">{{ $page }}</a></li>
-                    @endif
-                @endforeach
-            @endif
+            <ul role="navigation">
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <li class="active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                        @else
+                            <li><a href="{{ $url }}">{{ $page }}</a></li>
+                        @endif
+                    @endforeach
+                @endif
+            </ul>
         @endforeach
 
-        {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li>
                 <a class="btn btn-inverse next" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">NEWER</a>
-            </li>
         @else
-            <li class="disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-              <a class="btn btn-inverse next" href="">NEWER</a>
-            </li>
+                <a class="btn btn-inverse next disabled" href="#">NEWER</a>
 
-            </li>
         @endif
-    </ul>
+    </div>
 @endif
