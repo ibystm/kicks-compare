8910e906f52ff00af6e4d815e5641653cf10f09f
高橋康友 <>
2019-08-16 18:40:32 +0900

    seedするファイルを編集

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersTableSeeder::class);
-         $this->call(ShoesTableSeeder::class);
-         $this->call(CommentsTableSeeder::class);
+         // $this->call(ShoesTableSeeder::class);
+         // $this->call(CommentsTableSeeder::class);
          $this->call(ManufacturerTableSeeder::class);
     }
 }
