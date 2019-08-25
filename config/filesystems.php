8ebeb30f307e23a28e62014a2dbbf5ef19f3bef8
高橋康友 <>
2019-08-25 17:13:21 +0900

    パーミッション変更に不備があったので修正

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,11 +48,12 @@ return [
             'root' => storage_path('app'),
             'permissions' => [ // permissions 設定値を追記
                 'dir' => [
-                    'public'  => 0775, // public なディレクトリは775で作成
+                    'public'  => 0775, // public なディレクトリは775で作成 
                 ],
                 'file' => [
                     'public' => 0664, // public なファイルは664で作成
                 ],
+            ]
         ],
 
         'public' => [
