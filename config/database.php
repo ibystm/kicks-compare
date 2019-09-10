d86e6eb3b6acbce09941219d06e74e2742a24da6
高橋康友 <>
2019-09-10 23:39:55 +0900

    circleciのためのディレクトリ作成

--- config/database.php
+++ config/database.php
@@ -79,7 +79,21 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
         ],
-
+        'connections' => [
+            'circle_test' => [
+                'driver' => 'mysql',
+                'host' => '127.0.0.1',
+                'port' => '3306',
+                'database' => 'circle_test',
+                'username' => 'root',
+                'password' => '',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
+            ]
+        ]
     ],
 
     /*
